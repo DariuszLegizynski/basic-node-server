624d09f961822806c66dad2ea0531c1891d2017d
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2022-11-11 21:41:08 +0100

    trying to fix image issue

--- server.js
+++ server.js
@@ -1,30 +1,62 @@
+const fs = require('fs')
 const fsAsync = require('fs').promises
 const http = require('http')
-const requestListener = async (req, res) => {
-  console.log(req.url)
-  // console.log(req.headers.cookie)
-  let fileSystemPath = './public/'
-  switch(req.url){
-    case '/':
-      fileSystemPath += 'index.html'
-      break
-    case '/index2':
-      fileSystemPath += 'index2.html'
-      break
-    default:
-      fileSystemPath += '404.html'
-      break
-  }
+const path = require('path')
 
+const SERVER_PORT = 8080
+
+const readAsyncFile = async (fileSystemPath, encoding) => {
   try {
-    const encoding = 'utf-8'
-    const doc = await fsAsync.readFile(fileSystemPath, encoding)
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end(doc)
+    return await fsAsync.readFile(fileSystemPath, encoding)
   } catch (e) {
-    res.writeHead(400, { 'Content-Type': 'text/plain' })
-    res.end('NOT FOUND :( ')
+    res.writeHead(400, { 'Content-Type': contentType })
+    res.end('NOT FOUND: ', e)
   }
 }
+
+const requestListener = async (req, res) => {
+  let fileSystemPath = './public/'
+  console.log(req.url)
+
+  let contentType = 'text/html'
+  let encoding = 'utf-8'
+  let doc = null
+
+  if(req.url === '/') {
+    fileSystemPath += '/index.html'
+    doc = await readAsyncFile(fileSystemPath, encoding)
+    // res.statusCode = 200
+  } else if (req.url === '/index2.html') {
+    fileSystemPath += '/index2.html'
+    doc = await readAsyncFile(fileSystemPath, encoding)
+    // res.statusCode = 200
+  } else if (req.url.match(/.jpeg$/)) {
+    contentType = "image/jpeg"
+    encoding = null
+
+    fileSystemPath = "." + req.url
+    let jpgReadStream = null
+
+    try {
+      doc = await fsAsync.createReadStream(fileSystemPath)
+    } catch (e) {
+      res.writeHead(400, { 'Content-Type': contentType })
+      res.end('NOT FOUND: ', e)
+    }
+
+    // res.statusCode = 200
+
+    doc.pipe(res)
+  } else {
+    fileSystemPath += '/404.html'
+    res.statusCode = 404
+  }
+
+  res.writeHead(200, { 'Content-Type': contentType })
+  res.end(doc)
+}
+
 const server = http.createServer(requestListener)
-server.listen(8080)
+server.listen(SERVER_PORT, () => {
+  console.log(`Server running on PORT ${SERVER_PORT}`)
+})
