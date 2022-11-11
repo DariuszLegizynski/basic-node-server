7ccc96319b65cdb3a16ecde27631ac70291a4ca6
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2022-11-11 21:42:10 +0100

    return back to functioning code

--- server.js
+++ server.js
@@ -5,55 +5,42 @@ const path = require('path')
 
 const SERVER_PORT = 8080
 
-const readAsyncFile = async (fileSystemPath, encoding) => {
-  try {
-    return await fsAsync.readFile(fileSystemPath, encoding)
-  } catch (e) {
-    res.writeHead(400, { 'Content-Type': contentType })
-    res.end('NOT FOUND: ', e)
-  }
-}
-
 const requestListener = async (req, res) => {
   let fileSystemPath = './public/'
   console.log(req.url)
 
   let contentType = 'text/html'
   let encoding = 'utf-8'
-  let doc = null
 
   if(req.url === '/') {
     fileSystemPath += '/index.html'
-    doc = await readAsyncFile(fileSystemPath, encoding)
-    // res.statusCode = 200
+    res.statusCode = 200
   } else if (req.url === '/index2.html') {
     fileSystemPath += '/index2.html'
-    doc = await readAsyncFile(fileSystemPath, encoding)
-    // res.statusCode = 200
+    res.statusCode = 200
   } else if (req.url.match(/.jpeg$/)) {
+    console.log("have a jpeg")
     contentType = "image/jpeg"
+    fileSystemPath = "." + req.url
+    let jpgReadStream = fs.createReadStream(fileSystemPath)
+
+    res.statusCode = 200
     encoding = null
 
-    fileSystemPath = "." + req.url
-    let jpgReadStream = null
-
-    try {
-      doc = await fsAsync.createReadStream(fileSystemPath)
-    } catch (e) {
-      res.writeHead(400, { 'Content-Type': contentType })
-      res.end('NOT FOUND: ', e)
-    }
-
-    // res.statusCode = 200
-
-    doc.pipe(res)
+    jpgReadStream.pipe(res)
   } else {
     fileSystemPath += '/404.html'
     res.statusCode = 404
   }
 
-  res.writeHead(200, { 'Content-Type': contentType })
-  res.end(doc)
+  try {
+    const doc = await fsAsync.readFile(fileSystemPath, encoding)
+    res.writeHead(200, { 'Content-Type': contentType })
+    res.end(doc)
+  } catch (e) {
+    res.writeHead(400, { 'Content-Type': contentType })
+    res.end('NOT FOUND :( ')
+  }
 }
 
 const server = http.createServer(requestListener)
