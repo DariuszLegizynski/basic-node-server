44637f80fea25a44f90132f7de225deaaf47ecdc
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2022-12-12 15:45:42 +0100

    add eslint

--- server.js
+++ server.js
@@ -5,46 +5,47 @@ const SERVER_PORT = 3000
 
 const SERVER_TIME = new Date().toUTCString()
 
-parseCookies = str => {
-  let rx = /([^;=\s]*)=([^;]*)/g
-  let obj = {}
-  for (let m; m = rx.exec(str); )
-    obj[m[1]] = decodeURIComponent(m[2])
-  return obj
-}
+// const parseCookies = str => {
+//   const rx = /([^;=\s]*)=([^;]*)/g
+//   const obj = {}
+//   for (let m; m === rx.exec(str);)
+//     obj[m[1]] = decodeURIComponent(m[2])
+//   return obj
+// }
 
-stringifyCookies = cookies => {
-  return Object.entries(cookies)
-    .map(([k,v]) => k + '=' + encodeURIComponent(v))
-    .join('; ')
-}
+// const stringifyCookies = cookies => {
+//   return Object.entries(cookies)
+//     .map(([k, v]) => k + '=' + encodeURIComponent(v))
+//     .join('; ')
+// }
 
 const requestListener = async (req, res) => {
-  let fileSystemPath = './public'
-  console.log("from the listener: ", SERVER_TIME)
+  req.url = 'index.html'
+  let fileSystemPath = `./public/${req.url}`
 
-  console.log("req: ", req.headers)
+  console.log('req: ', req.headers)
+  // console.log("res: ", res)
 
   let contentType = 'text/html'
   let encoding = 'utf-8'
 
   if (req.url.match(/.js$/)) {
-    contentType = "application/javascript"
+    contentType = 'application/javascript'
     fileSystemPath += req.url
   } else if (req.url.match(/.css$/)) {
-    contentType = "text/css"
+    contentType = 'text/css'
     fileSystemPath += req.url
   } else if (req.url === '/') {
     fileSystemPath += '/index.html'
   } else if (req.url === '/index2') {
     fileSystemPath += '/index2.html'
   } else if (req.url.match(/.jpeg$/)) {
-    contentType = "image/jpeg"
-    fileSystemPath = "." + req.url
+    contentType = 'image/jpeg'
+    fileSystemPath = '.' + req.url
     encoding = null
   } else if (req.url.match(/.gif$/)) {
-    contentType = "image/gif"
-    fileSystemPath = "." + req.url
+    contentType = 'image/gif'
+    fileSystemPath = '.' + req.url
     encoding = null
   } else {
     fileSystemPath += '/404.html'
@@ -61,12 +62,12 @@ const requestListener = async (req, res) => {
     res.end('NOT FOUND :( ')
   }
 
-  if(req.headers["if-modified-since"] === SERVER_TIME) {
+  if (req.headers['if-modified-since'] === SERVER_TIME) {
     res.writeHead(304)
   } else {
-    res.writeHead(200, { "Last-Modified": SERVER_TIME, 'Content-Type': contentType })
+    res.writeHead(200, { 'Last-Modified': SERVER_TIME, 'Content-Type': contentType })
   }
-  
+
   res.end(doc)
 }
 
